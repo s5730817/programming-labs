2641994986967d900df57b68e7389889fa969be3
s5730817 <s5730817@bournemouth.ac.uk>
2024-10-10 14:22:10 +0100

    Homework update

--- lab2_hw.py
+++ lab2_hw.py
@@ -1,3 +1,5 @@
+from random import randint
+
 number = int(input("Enter number of rows >>> "))
 max_num_len = len(str(number))
 
@@ -19,3 +21,44 @@ for i in range(number):
 	print(space_start, end="")
 	print(*([i+1] * (i+1)), sep=num_sep, end="\n")
 	
+
+#############################################################################
+
+number = int(input("Enter number >>> "))
+while number >= 10:
+	number //= 10
+	
+print(number)
+
+############################################################################
+
+nums = []
+number = ''
+
+# User input and list generation
+while number != 'c':
+	number = input("Enter a number or 'c' to stop or 'r' to generate list >>> ")	
+	if number == 'c':
+		break	
+	if number == 'r':
+		list_len = int(input("How many numbers do you want to be in the list? >>> "))
+		for i in range(list_len):
+			nums.append(randint(-100, 100))
+		break
+	try:
+		number = int(number)
+		nums.append(number)	
+	except:
+		print("Not a number, try again!")
+		
+# Selection sort
+for i in range(len(nums)):
+	min_idx = i
+	for j in range(i+1, len(nums)):
+		if nums[min_idx] > nums[j]:
+			min_idx = j
+
+	nums[min_idx], nums[i] = nums[i], nums[min_idx]
+
+print(*nums)
+		
